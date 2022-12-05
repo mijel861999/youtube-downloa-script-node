c6749531bb8979cc71190fd51004780955b6a8cd
mijel <mijel.dev@gmail.com>
2022-12-04 22:10:38 -0500

    feat: creando script para descargar videos

---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const prompt = require('prompt-sync')()
+const ytdl = require('ytdl-core');
+// TypeScript: import ytdl from 'ytdl-core'; with --esModuleInterop
+// TypeScript: import * as ytdl from 'ytdl-core'; with --allowSyntheticDefaultImports
+// TypeScript: import ytdl = require('ytdl-core'); with neither of the above
+	//
+	//
+
+const link = prompt("Ingresa el enlace del video: ");
+
+console.log("El enlace es: " + link)
+
+ytdl(link)
+  .pipe(fs.createWriteStream('/home/mijel/Descargas/video1.mp4'));
+
+
+console.log("Video descargado")
